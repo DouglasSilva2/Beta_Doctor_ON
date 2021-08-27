47f01b4e7fa7114d3bd1c9bec6653ed64b77bbad
Douglas Silva <douguinhadod@hotmail.com>
2021-08-27 04:50:39 -0300

    Update agendamentos.php

--- doctorOn.html5/agendamentos.php
+++ doctorOn.html5/agendamentos.php
@@ -7,7 +7,7 @@
 <?php
 $host = "localhost";
 $user = "root";
-$pass = "Hidan@dog";
+$pass = "*******";
 $banco = "cadastro";
 $conexao = mysql_connect($host, $user, $pass) or die(mysql_error());
 mysql_select_db($banco) or die(mysql_error());
